25eadb3641e7eaabf08084fd240c435c76a0f339
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-06-15 00:59:10 +0200

    SCSetting recode, added strings and booleans

---
+++ src/es/xdec0de/securitycore/api/SCSetting.java
@@ -0,0 +1,111 @@
+package es.xdec0de.securitycore.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+public enum SCSetting {
+
+	AHS_ENABLED("AHS.Enabled", SettingType.BOOLEAN),
+	AHS_BYPASS_PERM("AHS.BypassPerm", SettingType.STRING),
+	AHS_NOTIFY_PERM("AHS.Notify.Permission", SettingType.STRING),
+	AHS_NOTIFY_CONSOLE("AHS.Notify.Console", SettingType.BOOLEAN),
+	AHS_NOTIFY_PLAYERS("AHS.Notify.Players", SettingType.BOOLEAN),
+
+	ANTITAB_ENABLED("AntiTab.Enabled", SettingType.BOOLEAN),
+	ANTITAB_BYPASS_PERM("AntiTab.BypassPerm", SettingType.STRING),
+	ANTITAB_HIDE_ALL("AntiTab.Hide.All", SettingType.BOOLEAN),
+	ANTITAB_HIDE_CONTAINS("AntiTab.Hide.ListContains", SettingType.STRING_LIST),
+	ANTITAB_HIDE_EXACT("AntiTab.Hide.ListExact", SettingType.STRING_LIST),
+
+	WARNCMDS_ENABLED("WarnCMDs.Enabled", SettingType.BOOLEAN),
+	WARNCMDS_BYPASS_PERM("WarnCMDs.BypassPerm", SettingType.STRING),
+	WARNCMDS_NOTIFY_PERM("WarnCMDs.Notify.Permission", SettingType.STRING),
+	WARNCMDS_NOTIFY_CONSOLE("WarnCMDs.Notify.Console", SettingType.BOOLEAN),
+	WARNCMDS_NOTIFY_PLAYERS("WarnCMDs.Notify.Players", SettingType.BOOLEAN),
+	WARNCMDS_LIST("WarnCMDs.List", SettingType.STRING_LIST),
+
+	BLOCKEDCMDS_ENABLED("BlockedCMDs.Enabled", SettingType.BOOLEAN),
+	BLOCKEDCMDS_BYPASS_PERM("BlockedCMDs.BypassPerm", SettingType.STRING),
+	BLOCKEDCMDS_NOTIFY_PERM("BlockedCMDs.Notify.Permission", SettingType.STRING),
+	BLOCKEDCMDS_NOTIFY_CONSOLE("BlockedCMDs.Notify.Console", SettingType.BOOLEAN),
+	BLOCKEDCMDS_NOTIFY_PLAYERS("BlockedCMDs.Notify.Players", SettingType.BOOLEAN),
+	BLOCKEDCMDS_LIST("BlockedCMDs.List", SettingType.STRING_LIST),
+
+	RELOAD_PERM("ReloadPerm", SettingType.STRING),
+
+	REDSTONELIMITER_ENABLED("RedstoneLimiter.Enabled", SettingType.BOOLEAN),
+	REDSTONELIMITER_MAX_PPS("RedstoneLimiter.Enabled", SettingType.INT) {
+		@Override
+		public boolean allows(String value) {
+			return StringUtils.isNumeric(value) ? Integer.valueOf(value) > 0 : false;
+		}
+	},
+
+	SCSETTING_PERM("SCSetting.Permission", SettingType.STRING);
+
+	private final String path;
+	private final SettingType type;
+
+	private SCSetting(String path, SettingType type) {
+		this.path = path;
+		this.type = type;
+	}
+
+	public static SCSetting of(String name) {
+		if (name == null || name.isEmpty())
+			return null;
+		try {
+			return valueOf(name);
+		} catch (IllegalArgumentException ex) {
+			return null;
+		}
+	}
+
+	public static List<SCSetting> ofType(SettingType type) {
+		List<SCSetting> res = new ArrayList<>();
+		for (SCSetting setting : SCSetting.values())
+			if (setting.getType().equals(type))
+				res.add(setting);
+		return res;
+	}
+
+	public static List<String> namesOfType(SettingType type) {
+		List<String> res = new ArrayList<>();
+		for (SCSetting setting : SCSetting.values())
+			if (setting.getType().equals(type))
+				res.add(setting.name());
+		return res;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public SettingType getType() {
+		return type;
+	}
+
+	public enum SettingType {
+		/** Represents a boolean value, true or false.*/
+		BOOLEAN,
+		/** Represents a positive integer value with a predefined range.*/
+		INT,
+		/** Represents a String.*/
+		STRING,
+		/** Represents a String list.*/
+		STRING_LIST;
+
+	}
+
+	public boolean allows(String value) {
+		if (getType().equals(SettingType.STRING) || getType().equals(SettingType.STRING_LIST))
+			return value != null && !value.isEmpty();
+		String val = value.toLowerCase();
+		if (getType().equals(SettingType.BOOLEAN))
+			return val.equals("true") || val.equals("false");
+		else
+			return StringUtils.isNumeric(val);
+	}
+}
--- src/es/xdec0de/securitycore/features/SettingChanger.java
+++ src/es/xdec0de/securitycore/features/SettingChanger.java
@@ -4,19 +4,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 
 import es.xdec0de.securitycore.SecurityCore;
+import es.xdec0de.securitycore.api.SCSetting;
+import es.xdec0de.securitycore.api.SCSetting.SettingType;
 
 public class SettingChanger implements TabExecutor {
 
 	private final SecurityCore plugin;
-	private List<String> ints = new ArrayList<>();
-
-	// /scsetting setint <path> <value>
 
 	public SettingChanger(SecurityCore plugin) {
 		this.plugin = plugin;
@@ -24,53 +22,69 @@ public class SettingChanger implements TabExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		initialize();
 		if (!plugin.getCfg().hasPermission("SCSetting.Permission", sender, true))
 			return true;
 		if (args.length == 3) {
-			switch (args[0].toLowerCase()) {
-			case "setint": handleInt(sender, args); break;
-			default: plugin.getMessages().send("SCSetting.Usage", sender); break;
-			}
+			String mode = args[0].toLowerCase();
+			SettingType type = getTypeFromCmd(mode);
+			if (type != null && mode.contains("set"))
+				return handleSet(sender, args, type); // Always returns true.
 		}
+		plugin.getMessages().send("SCSetting.Usage", sender);
 		return true;
 	}
 
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
-		initialize();
 		if (!plugin.getCfg().hasPermission("SCSetting.Permission", sender, false))
 			return new ArrayList<>();
 		if (args.length == 1)
-			return Arrays.asList("setint");
+			return Arrays.asList("setint", "setbool", "setstr");
 		if (args.length == 2) {
 			String mode = args[0].toLowerCase();
 			if (mode.equals("setint"))
-				return ints;
+				return SCSetting.namesOfType(SettingType.INT);
+			else if (mode.equals("setbool") || mode.equals("setboolean"))
+				return SCSetting.namesOfType(SettingType.BOOLEAN);
+			else if (mode.equals("setstr") || mode.equals("setstring"))
+				return SCSetting.namesOfType(SettingType.STRING);
 			return new ArrayList<>();
 		}
-		if (args.length == 3)
-			return Arrays.asList("value");
+		if (args.length == 3) {
+			String mode = args[0].toLowerCase();
+			boolean isBool = mode.equals("setbool") || mode.equals("setboolean");
+			return isBool ? Arrays.asList("true", "false") : Arrays.asList("value");
+		}
 		return null;
 	}
 
-	private void handleInt(CommandSender sender, String[] args) {
-		String path = args[1];
-		int value = StringUtils.isNumeric(args[2]) ? Integer.valueOf(args[2]) : 0;
-		if (!ints.contains(path))
-			plugin.getMessages().send("SCSetting.InvalidPath", sender);
-		else if (value <= 0)
-			plugin.getMessages().send("SCSetting.IntInvalid", sender);
+	private boolean handleSet(CommandSender sender, String[] args, SettingType type) {
+		SCSetting setting = SCSetting.of(args[1]);
+		String value = args[2];
+		if (setting == null)
+			plugin.getMessages().send("SCSetting.InvalidSetting", sender);
+		else if (!setting.allows(value))
+			plugin.getMessages().send("SCSetting.InvalidValue", sender);
 		else {
-			plugin.getCfg().getFile().set(path, value);
+			if (type.equals(SettingType.INT))
+				plugin.getCfg().getFile().set(setting.getPath(), Integer.valueOf(value));
+			else if (type.equals(SettingType.BOOLEAN))
+				plugin.getCfg().getFile().set(setting.getPath(), Boolean.valueOf(value));
+			else
+				plugin.getCfg().getFile().set(setting.getPath(), value);
 			plugin.getCfg().save();
 			plugin.getCfg().reload(false);
-			plugin.getMessages().send("SCSetting.Changed", sender, "%path%", path, "%value%", args[2]);
+			plugin.getMessages().send("SCSetting.Changed", sender, "%setting%", setting.name(), "%value%", value);
 		}
+		return true;
 	}
 
-	private void initialize() {
-		if (ints.isEmpty())
-			ints = Arrays.asList("RedstoneLimiter.MaxPulsesPerSecond");
+	private SettingType getTypeFromCmd(String mode) { // mode is already lower case
+		switch (mode) {
+		case "setint": return SettingType.INT;
+		case "setboolean": case "setbool": return SettingType.BOOLEAN;
+		case "setstr": case "setstring": return SettingType.STRING;
+		default: return null;
+		}
 	}
 }
